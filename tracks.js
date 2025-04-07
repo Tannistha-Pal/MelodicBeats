a4ec801271ecfde5684b52ba52bb931fb286d230
TANNISTHA PAL <tannisthapal6@gmail.com>
2025-04-07 20:27:29 +0530

    Music Player Files added

---
+++ tracks.js
@@ -0,0 +1,67 @@
+const tracks = [
+  { 
+    artwork:
+    "mann mera img.jpg",
+    name: "Mann Mera",
+    artist: "Gajend Verma",
+    path: "128-Mann Mera - Table No. 21 128 Kbps.mp3",
+  },
+  {
+    artwork:
+      "1920 - Evil Returns img.jpg",
+    name: "Apna Mujhe Tu Lagaa",
+    artist: "Sonu Nigam",
+    path: "128-Apnaa Mujhe Tu Lagaa - 1920 - Evil Returns 128 Kbps.mp3",
+  },
+  {
+    artwork:
+      "Bhool_Bhulaiyaa_3_poster.jpg",
+    name: "Ami Je Tomar 3.0",
+    artist: "Shreya Ghoshal",
+    path: "128-Ami Je Tomar 3.0 - Bhool Bhulaiyaa 3 128 Kbps.mp3",
+  },
+    {
+      artwork:
+        "Perfect Song Image.webp",
+      name: "Perfect",
+      artist: "Ed Sheeran",
+      path: "Perfect-(Mr-Jat.in).mp3",
+    },
+    {
+      artwork:
+        "arctic monkey img.jpg",
+      name: "Wanna Be Yours",
+      artist: "Arctic Monkeys",
+      path: "I-Wanna-Be-Yours(Pagal-World.Com.In).mp3",
+    },
+   
+    {
+      artwork:
+        "Hamari Adhuri Kahani img.jpg",
+      name: "Hasi",
+      artist: "Ami Mishra",
+      path: "Hasi (Male Version).mp3",
+    },
+
+    {
+      artwork:
+        "aashiqui 2.jpg",
+      name: "Tum Hi Ho",
+      artist: "Arijit Singh",
+      path: "128-Tum Hi Ho - Aashiqui 2 128 Kbps.mp3",
+    },
+    {
+      artwork:
+        "OM shanti om img.jpg",
+      name: "Main Agar Kahoon",
+      artist: "Shreya Ghoshal & Sonu Nigam",
+      path: "Main Agar Kahoon.mp3",
+    },
+    {
+      artwork:
+        "Bhool_bhulaiyaa.jpg",
+      name: "Mere Dholna",
+      artist: "Shreya Ghoshal",
+      path: "Mere Dholna.mp3",
+    },
+  ];
